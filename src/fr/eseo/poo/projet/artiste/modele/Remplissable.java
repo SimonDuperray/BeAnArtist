57e390e2de2394477a7bc003f3903b9d4d7b585c
Kartodix <64204457+Kartodix@users.noreply.github.com>
2021-04-04 19:47:24 +0200

    3.6.1.1

---
+++ src/fr/eseo/poo/projet/artiste/modele/Remplissable.java
@@ -0,0 +1,9 @@
+package fr.eseo.poo.projet.artiste.modele;
+
+public interface Remplissable {
+   // getter
+   public boolean estRempli();
+
+   // setter
+   public void setRempli(boolean isRempli);
+}
